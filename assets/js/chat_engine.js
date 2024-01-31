7f7c2fd60499fd614a91ca39965c18a49a8121c1
S SAI NISSHITH <sainisshith@gmail.com>
2024-02-01 01:19:04 +0530

    Removed Folders

--- assets/js/chat_engine.js
+++ assets/js/chat_engine.js
@@ -2,7 +2,7 @@ class ChatEngine{
     constructor(chatBoxId,userEmail){
         this.chatBox = $(`#${chatBoxId}`);
         this.userEmail = userEmail;
-        this.socket = io.connect('http://localhost:5000');
+        this.socket = io.connect('http://65.1.148.117:5000');
         if(this.userEmail){
             this.connectionHandler();
         }
